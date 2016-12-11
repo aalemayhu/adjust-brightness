b234d3fdb22dbf9af5637e3497ac90e59f3a4cc3
Alexander Alemayhu <alexander@alemayhu.com>
2016-12-11 11:21:37 +0100

    improvements after using it
    o move the logging into a verbose mode.
    o remove interactive part.
    o use argument to set brightness.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- adjust-brightness.c
+++ adjust-brightness.c
@@ -15,18 +15,31 @@ int main(int argc, const char *argv[])
 {
 	struct udev_list_entry *devices, *dev_list_entry;
 	struct udev_enumerate *enumerate;
-	bool should_turn_off = false;
+	const char *new_brightness;
 	struct udev_device *dev;
+	bool verbose = false;
 	struct udev *udev;
+	int i;
+
+	if (2 > argc) {
+		printf("%s: Please provide a value to use.\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	new_brightness = argv[1];
+
+	for (i = 0; i < argc; i++) {
+		if (!strcmp(argv[i], "-v")) {
+			verbose = true;
+			printf("verbose mode on\n");
+			printf("new_brightness will be %s\n", new_brightness);
+		}
+	}
 
 	udev = udev_new();
 	if (!udev) {
 		perror("udev_new");
 		return EXIT_FAILURE;
 	}
-
-	if (argc > 1)
-		should_turn_off = !strcmp(argv[1], "off");
 
 	enumerate = udev_enumerate_new(udev);
 
@@ -42,50 +55,22 @@ int main(int argc, const char *argv[])
 		actual_brightness = udev_device_get_sysattr_value(dev, "actual_brightness");
 		max_brightness = udev_device_get_sysattr_value(dev, "max_brightness");
 
-		printf("path=%s\n", path);
-		printf("sysname=%s\n", udev_device_get_sysname(dev));
-		printf("syspath=%s\n", udev_device_get_syspath(dev));
-		printf("actual_brightness: %s\n", actual_brightness);
-		printf("max_brightness: %s\n", max_brightness);
+		if (verbose) {
+			printf("path=%s\n", path);
+			printf("sysname=%s\n", udev_device_get_sysname(dev));
+			printf("syspath=%s\n", udev_device_get_syspath(dev));
+			printf("actual_brightness: %s\n", actual_brightness);
+			printf("max_brightness: %s\n", max_brightness);
+		}
 
-		if (should_turn_off) {
-			if (0 > set_brightness(dev, "0"))
-				perror("set_brightness");
+		if (!strcmp(new_brightness, max_brightness)) {
+			if (verbose)
+				printf("new_brightness is equal to max_brightness\n");
 			break;
 		}
 
-		printf("supported actions + and -\n");
-		char c;
-		do {
-			do {
-				c = getchar();
-			} while (c == ' ' || c == '\n');
-
-			int current_brightness;
-			current_brightness =
-				atoi(udev_device_get_sysattr_value(dev,
-								   "brightness"));
-
-			if (c == '+')
-				current_brightness++;
-			else if (c == '-')
-				current_brightness--;
-			else
-				break;
-
-			char new_brightness[1024];
-			sprintf(new_brightness, "%d", current_brightness);
-
-			printf("new_brightness=%s\n", new_brightness);
-
-			if (!strcmp(new_brightness, max_brightness)) {
-				printf("new_brightness is equal to max_brightness\n");
-				break;
-			}
-
-			if (0 > set_brightness(dev, new_brightness))
-				perror("set_brightness");
-		} while (c != 'q');
+		if (0 > set_brightness(dev, (char *)new_brightness))
+			perror("set_brightness");
 	}
 
 	udev_enumerate_unref(enumerate);
